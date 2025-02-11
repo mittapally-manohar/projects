201f33d1b4387a989d889e0c334cec3c596adfb3
Manohar_Mittapally <mittapallymanohar194@gmail.com>
2025-02-12 01:28:07 +0530

    User Story: User can enter a short URL and get redirected to the original long URL

--- urlshortner/src/main/java/com/practice/projects/urlshortner/UrlController.java
+++ urlshortner/src/main/java/com/practice/projects/urlshortner/UrlController.java
@@ -1,22 +1,35 @@
 package com.practice.projects.urlshortner;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/url")
 public class UrlController {
 
     @Autowired
     private UrlService urlService;
 
-    @PostMapping("/shorten")
+    @PostMapping("/url/shorten")
     public String shortenUrl(@RequestParam String longUrl) {
         return urlService.shortenUrl(longUrl);
     }
 
-    @GetMapping("/expand")
+    @GetMapping("/url/expand")
     public String getLongUrl(@RequestParam String shortUrl) {
         return urlService.getLongUrl(shortUrl);
+    }
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Void> redirectToLongUrl(@PathVariable String shortUrl) {
+        String longUrl = urlService.getLongUrl(shortUrl);
+        if(longUrl != null) {
+            return ResponseEntity.status(HttpStatus.FOUND)
+                    .header("Location", longUrl)
+                    .build();
+        } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
     }
 }
